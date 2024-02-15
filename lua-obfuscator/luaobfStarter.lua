ae23917f1d4b568915bd8bd493bc730101858195
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-02-15 12:21:42 +0500

    Create luaobfStarter.lua

---
+++ lua-obfuscator/luaobfStarter.lua
@@ -0,0 +1,11 @@
+require("lua-obfuscator.chaselua") 
+local i = 1
+ if i then print("done!!!!") end
+    local p = 0
+    if p then print("Welcome!!!")
+end
+local cd = "PROMETHEUS CD"
+if cd then print(cd)
+end
+
+return luaobfStarter
