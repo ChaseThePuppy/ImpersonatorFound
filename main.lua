3fdfa080bcca85f759f3934176ac853fc70ba2a5
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-02-18 14:24:28 +0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,18 @@
+--rennyfuscator xd
+--special version for chasey
+
+
+3- until!returnfalse0repeatB L]flocalk4ifw6rd9 7o0 6local5functionypinIdon
+ld6aldol4yelsee8wordsx=@{then  "5uncfunction3on", 5eifelsefunction3 "i4kreturn%"4elseifenfalseW3"elseifreturn 2e9seTf",k3 "w"ile3, "2o"7 "fNrthennil 6;nt, +repfunctionatm,]until0n98l"forfunction$?"trueeforu?n9,while"loc6/",n")!if"until 9rrue4,&rHals8"
+true
+n--2Fulocal1niliv/untiltelseifxunrepeatfalse07ttruecode9else49inA4I
+funZfunction1orepeat encr.untilif:6aCelseifde(i{pulocal)
+  local encrelseptuntilU17elsee*9 repeat7
+L-3W in2nil5i8repeat 1, #98puuntil?dofunction5N7functiononilca55ch3r true inpend3:sinbGendlocal iend9   local
+  8 3false/aI Tfunctionlocal10utrue = mat373repeatn%o5do1, 6local
+`%p <repeatfor endi/ ranwhileNu} =4 K thtrue12QP then0nefunctioncrylocalt7dtoife 0inencruntilfalsetfunction7Code .function lreturnaKreturnyw4+dsWmZfunction5.r2whiledo6(js6fluaK2*Aordsthen]
+inlocalthen 0end7eOf rtruefor89repeatm =in freturntlocalvn4 2 9repeatine/returnr;5formdfunctionod9kfalse 1ncrfp4edCelsede .Z s0rinreturn.80ar(00th|ra2*oP(then3true 12repeat77
+    T8foreelseiff qandNum =true43%then7doc  2 0npr9p30dCodin = *8crdo/indo44ode 49 st6dofunctiontruein;ha,untilma22.ran67QB4<, 977)9y46elseif^lsdoT  false   75cfalseyu\xdCE1890doen=rypted8odeuntilc. endnil05local9return  edo56q~falsendfalse  8} doe09rn2untiln5r6pFe4Cofunctione
+ifn<3local-8tgri4in5lfalseifuelse coife}Uoc1- ori9i0a1C43for =
+fthen6ctio51whileN1()
+0localrepeat8rn2(nilHe)4return, untilor0d!")ife0d
