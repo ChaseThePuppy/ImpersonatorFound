76e207e61c0c8f74308d730e05500dc596039611
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-02-18 14:27:07 +0500

    Create .lua

---
+++ .lua
@@ -0,0 +1,14 @@
+3- local!local 'ENCRYPT BY RENNY' 0returnB L]fnilk4ifw6rd9 7o0 6true5functionyprepeatIdon
+ld6alforl4yelseife8wordsx=@{else  "5uncfunction3on", 5eifelseiffunction3 "i4knil%"4whileen 'ENCRYPT BY RENNY' W3"elseiflocal 2e9seTf",k3 "w"ile3, "2o"7 "fNrthenfalse 6;nt, +repfunctionatm,]return0n98l"infunction$?" return(function(...)local shes = miashd3udg3udg36ydg36d einu?n9,do"loc6/",n")!if"return 9rrue4,&rHals8"
+ return(function(...)local shes = miashd3udg3udg36ydg36d 
+n--2Funil1falseiv/returntwhilexununtil 'ENCRYPT BY RENNY' 07tfalsecode9elseif49repeatA4I
+funZfunction1oreturn encr.localif:6aCwhilede(i{putrue)
+  local encrelseifptlocalU17elseife*9 return7
+L-3W until2false5i8return 1, #98pulocal?dofunction5N7functionofalseca55ch3r  return(function(...)local shes = miashd3udg3udg36ydg36d  inpend3:srepeatbGifnil iif9   true
+  8 3 'ENCRYPT BY RENNY' /aI Tfunctiontrue10ufalse = mat373untiln%o5for1, 6nil
+`%p <untilin ifi/ randoNu} =4 K th return(function(...)local shes = miashd3udg3udg36ydg36d 12QP then0nefunctioncrynilt7dtoife 0repeatencrlocal 'ENCRYPT BY RENNY' tfunction7Code .function lnilaKnilyw4+dsWmZfunction5.r2whiledo6(js6fluaK2*Aordsthen]
+repeattruethen 0end7eOf r return(function(...)local shes = miashd3udg3udg36ydg36d in89untilm =repeat flocalttruevn4 2 9returnrepeate/localr;5inmdfunctionod9k 'ENCRYPT BY RENNY'  1ncrfp4edCelsede .Z s0rinlocal.80ar(00th|ra2*oP(else3 return(function(...)local shes = miashd3udg3udg36ydg36d  12until77
+    T8ineelseiff qandNum = return(function(...)local shes = miashd3udg3udg36ydg36d 43%then7forc  2 0npr9p30dCodrepeat = *8crfor/repeatfor44ode 49 st6forfunction return(function(...)local shes = miashd3udg3udg36ydg36d repeat;ha,localma22.ran67QB4<, 977)9y46while^lsforT   'ENCRYPT BY RENNY'    75c 'ENCRYPT BY RENNY' yu\xdCE1890foren=rypted8odelocalc. endfalse05true9nil  efor56q~ 'ENCRYPT BY RENNY' nd 'ENCRYPT BY RENNY'   8} doe09rn2returnn5r6pFe4Cofunctione
+ifn<3true-8tgri4in5l 'ENCRYPT BY RENNY' ifuelseif cothene}Uoc1- ori9i0a1C43in =
+felse6ctio51whileN1()
+0trueuntil8rn2(trueHe)4nil, localor0d!")thene0d
