4765954542060aa39782fdd8e3f13e096679cfdb
Chase The Puppy {{Active Dev, Official Dev, Coder , Developer}} <155614142+ChaseThePuppy@users.noreply.github.com>
2024-02-21 19:05:44 +0500

    Create antiPullRequest.lua

---
+++ antiPullRequest.lua
@@ -0,0 +1,14 @@
+--PSYCOPATH HUNT XD
+--renny
+local vald = globalValueSearch(...)
+local val = {}
+function val:init(...)
+  or vald:init()
+  local a = [[
+  print(..text..)
+  ]]
+  a = val.searchNumber("h3 234 43534 345 345 34 38 3y 4i 59 47 3u 4i 5iu 3o 38 49i")
+  if a then print("ANTI PULL REKUEST SUKSESFUL")
+  end
+  return val
+  
